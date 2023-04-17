ae2ee0aacb4bd4e8a50eec98e541e1965760f43d
Peterson A. Sena <petersonsena24@gmail.com>
2023-04-17 10:43:27 +0200

    Tema 3 Nivel 1 Exercise 4 arithmetic operations

---
+++ Tema_3/Nivel_1/Ejercicio4.php
@@ -0,0 +1,25 @@
+<?php
+echo "<left><h2><b>" . "Ejercicio 4" . "</b></h2></left>";
+//Declara dos variables X e Y de tipo int, dos variables N y M de tipo double y asigna a cada una un valor. A continuación, muestra por pantalla para X e Y: El valor de cada variable. La suma. El resto. El producto. El módulo.Por N y M realizarás lo mismo. Para todas las variables (X, Y, N, M) El doble de cada variable. La suma de todas las variables. El producto de todas las variables
+
+$x = 56;
+$y = 675;
+$n = 5.4;
+$m = 3.43;
+
+echo $x + $y . "<br>";
+echo $x - $y . "<br>";
+echo $x * $y . "<br>";
+echo $x % $y . "<br>";
+echo $n + $m . "<br>";
+echo $n - $m . "<br>";
+echo $n * $m . "<br>";
+echo $n % $m . "<br>";
+echo $x * 2 . "<br>";
+echo $y * 2 . "<br>";
+echo $n * 2 . "<br>";
+echo $m * 2 . "<br>";
+echo $x + $y + $n + $m . "<br";
+echo $x * $y * $n * $m . "<br>";
+
+?>
