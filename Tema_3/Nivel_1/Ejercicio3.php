c6ec578f0cad36f666a8d06d1317dd6bd7df447b
Peterson A. Sena <petersonsena24@gmail.com>
2023-04-17 10:41:52 +0200

    Tema 3 Nivel 1 Exercise 3 declare coonstant

---
+++ Tema_3/Nivel_1/Ejercicio3.php
@@ -0,0 +1,8 @@
+<?php
+echo "<left><h2><b>" . "Ejercicio 3" . "</b></h2></left>";
+//Crea una constante que contenga tu nombre y lo imprima por pantalla. Tienes que imprimirla con formato de título y letra en negrita en HTML.
+define("NOMBRE", "Peterson");
+const NOMBRE_2 = "Peterson Andres"; //Nueva forma de definir constante
+echo "<h1><b>" .NOMBRE  ."</b></h1>"; //Funciones de html se pueden utilizar en el codigo <b><h1>
+echo "<h1><b>". NOMBRE_2 . "</b></h1>"; //Funciones de html se pueden utilizar en el codigo <b><h1>
+?>
