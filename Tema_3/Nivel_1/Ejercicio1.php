34d229ef235e9eeea19b47a086defaf41de99127
Peterson A. Sena <petersonsena24@gmail.com>
2023-04-17 10:13:52 +0200

    Tema 3 Nivel 1 Exercise 1 declaring variables

---
+++ Tema_3/Nivel_1/Ejercicio1.php
@@ -0,0 +1,14 @@
+<?php
+echo "<center><h1><b>" . "Nivel 1" . "</b></h1></center>";
+echo "<left><h2><b>" . "Ejercicio1" . "</b></h2></left>";
+//Define una variable de cada tipo: integer,double,string y boolean. Después, imprímelas por pantalla.
+$numero = 5;
+$decimal = 4.56;
+$booleano = false;
+$cadena = "peter";
+$hola2 = "2";
+
+echo $numero ."<br>" . $decimal . "<br>". $booleano  . $cadena . "<br>";
+var_dump($booleano);
+
+?>
