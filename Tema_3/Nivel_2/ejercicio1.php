1cc4e84b5223c8d0d351ad51cfc106dc9a68d5d4
Peterson A. Sena <petersonsena24@gmail.com>
2023-05-03 16:54:36 +0200

    Tema 3 Nivel 2 Ex1 calculate equal values

---
+++ Tema_3/Nivel_2/ejercicio1.php
@@ -0,0 +1,9 @@
+<?php
+//Escribir un programa PHP para calcular la suma de los dos valores enteros dados. Si ambos valores son iguales, devuelve el doble de su suma.
+$num1 = 2;
+$num2 = 4;
+$num3 = 2;
+
+if($num1 == $num3){
+    echo $num1 + $num3 * 2;
+}
