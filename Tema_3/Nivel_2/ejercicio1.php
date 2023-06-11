c1d0b530fc987bcad7e01b21512068280d6bd587
Peterson A. Sena <petersonsena24@gmail.com>
2023-06-11 23:34:48 +0200

    completando ejericio

--- Tema_3/Nivel_2/ejercicio1.php
+++ Tema_3/Nivel_2/ejercicio1.php
@@ -1,9 +1,14 @@
 <?php
 //Escribir un programa PHP para calcular la suma de los dos valores enteros dados. Si ambos valores son iguales, devuelve el doble de su suma.
-$num1 = 2;
-$num2 = 4;
-$num3 = 2;
+$num1 =7;
 
-if($num1 == $num3){
-    echo $num1 + $num3 * 2;
+$num2 = 7;
+echo calcularSuma($num1, $num2);
+
+function calcularSuma($num1,$num2){
+    $total = $num1 + $num2;
+    if($num1 == $num2){
+        return $total *2;  
+    }
+
 }
