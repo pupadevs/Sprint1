5ff12e6d9a7d6e8f5c6885b10b0666dafee70d99
Peterson A. Sena <petersonsena24@gmail.com>
2023-07-16 22:22:35 +0200

    resolviendo parametro por defecto

--- Tema_3_Funcions/Nivel_1/Ejercicio4.php
+++ Tema_3_Funcions/Nivel_1/Ejercicio4.php
@@ -1,12 +1,14 @@
 <?php
 //Juego del escondite con parametro por defecto de 10
 echo "Juego deel escondite, empiezo a contar" . "<br>";
-$limite = 10;
-function escondite($limite){
-    
-    for($i = 0; $i <= $limite ; $i+=2){ //Ciclo for para contar de 2 en 2 i = 0, si i <= limite, incrementador i+=2
-        echo "Numero " . $i . "<br>";
-      }
+
+function escondite($limite = 10){
+    $num = 0;
+   while($num <= $limite){
+    echo "Numero $num". PHP_EOL;
+    $num++;
+
+   }echo "El escondiste donde estas";
 }
-echo escondite($limite);
+escondite();
 ?>
