92ec085dcfa93f9b5a6ca35238d9f1363b919bdd
Peterson A. Sena <petersonsena24@gmail.com>
2023-07-16 21:40:32 +0200

    Solvings errors

--- Tema_3/Nivel_2/ejercicio1.php
+++ Tema_3/Nivel_2/ejercicio1.php
@@ -2,13 +2,18 @@
 //Escribir un programa PHP para calcular la suma de los dos valores enteros dados. Si ambos valores son iguales, devuelve el doble de su suma.
 $num1 =7;
 
-$num2 = 7;
+$num2 = 4;
 echo calcularSuma($num1, $num2);
 
-function calcularSuma($num1,$num2){
+function calcularSuma(  $num1, $num2){
+    if(is_integer($num1) && is_integer($num2)){
     $total = $num1 + $num2;
     if($num1 == $num2){
         return $total *2;  
+    }else{
+        return "Los valores no son iguales";
     }
-
+    }else{
+        return  "No es un entero el dato introducido";
+    }
 }
