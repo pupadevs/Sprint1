cef20dab539dc4956d0c0dee4f73f464a20821ee
Peterson A. Sena <petersonsena24@gmail.com>
2023-07-16 21:45:32 +0200

    Level 3 ex2 repeats

--- Tema_3/Nivel_3/Ejercicio2.php
+++ Tema_3/Nivel_3/Ejercicio2.php
@@ -4,14 +4,17 @@ echo "<left><h2><b>" . "Ejercicio 2" .  "</b></h2></left>";
 $nums = array(1, 2, 3, 4, 2, 5, 6, 2, 7, 8, 2, 9, 2);
 $nombres = array("Jose", "Juan", "Pedro", "Juan", "Pedro", "Juan", "Jose");
 
-$rep2 = array_count_values($nombres);
-$rep = array_count_values($nums); //array count cuenta el numero de veces que un valor se repite y lo almaceno en la var $rep
-print_r($nums);
-foreach ($rep as $nums => $veces) { //for each donde recorre
-    echo "$nums se repite $veces veces <br>";
-}
-   foreach ($rep2 as $nombres => $veces2) {
+$nombresRep = array_count_values($nombres);
+  $numsRep = array_count_values($nums); //array count cuenta el numero de veces que un valor se repite y lo almaceno en la var $rep
+  print_r($rep) . "<br>";
+echo "<br>";
+  //print_r($rep) . "<br>";
+  foreach ($numsRep as $nums => $veces) { //for each donde recorre
+      echo "$nums se repite $veces veces <br>";
+  }
+   foreach ($nombresRep as $nombres => $veces2) {
      echo "Este nombre: $nombres se repite $veces2 veces <br>";
+
    }
 
 
