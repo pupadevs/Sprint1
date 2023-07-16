ab0fa59ad730daf86ec7f7a5aa63a7c1544d6ae5
Peterson A. Sena <petersonsena24@gmail.com>
2023-07-16 22:24:34 +0200

    resolviendo error

--- Tema_3_Funcions/Nivel_2/ejercicio2.php
+++ Tema_3_Funcions/Nivel_2/ejercicio2.php
@@ -1,20 +1,22 @@
 <?php
-//Escribe una función que determine la cantidad total a pagar por una llamada telefónica según las siguientes premisas: Toda llamada que dure menos de 3 minutos tiene un coste de 10 céntimos. Cada minuto adicional a partir de los 3 primeros es un paso de contador y cuesta 5 céntimos.
-$minutos = 20;
+//Escribe una función que determine la cantidad total a pagar por una llamada telefónica según las siguientes premisas: Toda llamada que dure menos de 3 duracionLllamada tiene un coste de 10 céntimos. Cada minuto adicional a partir de los 3 primeros es un paso de contador y cuesta 5 céntimos.
+$minutos = 3;
 
-function telefonica($minutos){
+function telefonica($duracionLllamada){
     $total = 0;
-    $cost = 0.10;
-    $cost2 = 0.05;
+    $costeInicial = 0.10;
+    $costeAdicional = 0.05;
     $limite = 3;
-    if($minutos  < $limite){
-       
-        $total = $minutos * $cost;
-        echo "El coste total de la llamada es: " . $total;
-    }else {
-        $total = $minutos * $cost2;
-       
+    if($duracionLllamada  > $limite){
+        $total = $costeInicial + $duracionLllamada * $costeAdicional;
         echo "El coste total de esta llamada es:$total €";
+       
+    }else {
+        $total += $costeInicial;
+        $totalFormat = number_format($total, 2);
+        echo "El coste total de la llamada es:  $totalFormat €";
+       
+       
     }
 } 
  telefonica($minutos);
